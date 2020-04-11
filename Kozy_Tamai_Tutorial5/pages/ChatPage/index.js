191ffbd95ceeed62e9aa633de0ca0aee730d2796
= <=>
2020-04-11 16:13:47 -0700

    Finished CustomButton

---
+++ Kozy_Tamai_Tutorial5/pages/ChatPage/index.js
@@ -0,0 +1,83 @@
+import React, {useState} from 'react';
+import './chatpage.css';
+import Chat from '../../comps/Chat';
+import CustomButton from '../../comps/CustomButton';
+import Header from '../../comps/Header';
+import Input from '../../comps/Input';
+
+/*
+var welcome_state = "Welcome to my App!"
+function setWelcome() {
+    welcome_state = "Start with sending a message.";
+    document.querySelector("#welcome").innerText = welcome_state;
+}
+*/
+
+//change useState
+//import custome button
+//if you clic the button, chnage the text and color of the custome button 
+
+const ChatPage =({}) => {
+    const [welcome, setWelcome] = useState("Welcome to my App!");
+    const[msg, setMsg] = useState("Please type something!");
+    const[resp, setResp] = useState("Let me respond to you!");
+    const[color, setColor] = useState("#33cc33");
+    const[text, setText] = useState("Click Here");
+
+    return <div>
+        <div id="welcome">
+            <Header fontSize={32} text={welcome} />
+        </div>
+
+        <div id="chats" onClick={()=>{
+            setWelcome("Start with sending a message.");
+        }}>
+            <Chat name={"User 1"} msg={msg}/>
+            <p></p>
+            <Chat img={"https://image.flaticon.com/icons/png/512/2068/2068998.png"} name={"Chat Bot"} backgroundColor={"#FAB"} msg={resp}/>
+            <p></p>
+        </div>
+
+        <div id="controls">
+            <Input onClick={(val)=>{
+                setMsg(val);
+                var new_resp = CheckResponse(val);
+
+                setResp(new_resp); //function 
+                
+                // if (val === "hi") {
+                //     setResp("I love pies");
+                // 
+
+            }}/>
+        </div>
+
+        <div id="custom_button">
+            <br/>
+            <CustomButton text={text} color={color} onClick={()=>{
+            setColor("#cc99ff");
+            setText("Yey!");
+        }}/>
+        </div>
+    </div>
+}
+
+function CheckResponse(inp){
+    switch(inp.toLowerCase()) {
+        case "hi":
+            return "I love pies";
+        
+        case "how are you?":
+            return "Great!";
+        
+        default:
+            return "Sorry, I don't understand!";
+    }
+}
+
+    ChatPage.defaultProps = {
+    
+
+    }
+
+    export default ChatPage;
---
+++ comps/CustomButton/index.js
@@ -0,0 +1,25 @@
+import React, {useState, useEffect} from 'react';
+import './cb.css';
+
+//Made the UI
+const CustomButton = ({text, color, onClick}) => <div 
+    style={{backgroundColor: color, boxShadow: "1px 1px 5px" + color}}
+    className="custom_button_box"
+    onClick={onClick}>
+    <div className="custom_button_box_inner">
+        {text}
+    </div>
+</div>
+
+
+function ButtonClick() {
+    alert("Clicked");
+}
+
+CustomButton.defaultProps = {
+    text:"Default Button",
+    color: "rgb(230, 175, 103)",
+    onClick: ButtonClick,
+}
+
+export default CustomButton;
